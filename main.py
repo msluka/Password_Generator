e9943e3300427d7cf122f2d28eaa15e1ac380250
Msluka <msluka.info@gmail.com>
2024-12-07 23:33:26 +0100

    Adding comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Pasword Generator
+#Password Generator
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -12,9 +12,9 @@ def  generate_password():
 
         try:
             pass_length = int(pass_length)
-            nr_of_letters = int(pass_length * 0.4)
-            nr_of_numbers = int(pass_length * 0.4)
-            nr_of_symbols = pass_length - (nr_of_letters + nr_of_numbers)
+            nr_of_letters = int(pass_length * 0.4) #40% letters
+            nr_of_numbers = int(pass_length * 0.4) #40% numbers
+            nr_of_symbols = pass_length - (nr_of_letters + nr_of_numbers) # the rest are symbols
 
             random_letters = random.choices(letters, k=nr_of_letters)
             random_numbers = random.choices(numbers, k=nr_of_numbers)
